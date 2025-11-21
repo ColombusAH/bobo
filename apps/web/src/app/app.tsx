02f9eedeb5582b9716cdaf2b8c8fa3cd626ba5b3
colombus <hattabavner@gmail.com>
2025-11-21 18:03:23 +0200

    add tailwind

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -5,6 +5,7 @@ import NxWelcome from './nx-welcome';
 export function App() {
   return (
     <div>
+      <h1 className="text-3xl font-bold underline text-red-500">Hello World</h1>
       <NxWelcome title="@org/web" />
     </div>
   );
